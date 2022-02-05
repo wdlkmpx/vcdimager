cf4c468ccd0902c9882b57f91cf1b11c4179de3b
wdlkmpx <wdlkmpx@gmail.com>
2022-02-05 16:13:15 -0500

    autogen.sh: don't call configure

--- autogen.sh
+++ autogen.sh
@@ -58,13 +58,6 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
-if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
-fi
-
 case $CC in
 xlc )
   am_opt=--include-deps;;
@@ -131,12 +124,4 @@ test -f $srcdir/docs/stamp-vti && rm $srcdir/docs/stamp-vti
 test -f $srcdir/docs/stamp-1 && rm $srcdir/docs/stamp-1
 test -f $srcdir/docs/stamp-2 && rm $srcdir/docs/stamp-2
   
-conf_flags="--enable-maintainer-mode" # --enable-compile-warnings #--enable-iso-c
 
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile $PKG_NAME
-else
-  echo Skipping configure process.
-fi
