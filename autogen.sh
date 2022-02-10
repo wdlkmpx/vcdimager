3b5a95c2eb62b0ceaa0d2ee0ecf44baba2b6c05e
wdlkmpx <wdlkmpx@gmail.com>
2022-02-10 09:47:24 -0500

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,78 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+case $1 in
+	po|pot|linguas) po/zzpo.sh $1 ; exit $? ;;
+	release|-release|--release)
+		pkg="$(grep -m 1 AC_INIT configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
+		ver="$(grep -m 1 AC_INIT configure.ac | cut -f 3 -d '[' | cut -f 1 -d ']')"
+		ver=$(echo $ver)
+		dir=${pkg}-${ver}
+		rm -rf ../${dir}
+		mkdir -p ../${dir}
+		cp -rf $(pwd)/* ../${dir}
+		( cd ../${dir} ; ./autogen.sh )
+		cd ..
+		tar -Jcf ${dir}.tar.xz ${dir}
+		exit $?
+		;;
+	"") ok=1 ;;
+	*)
+		echo "Unrecognized param: $1"
+		exit 1 ;;
+esac
+
+#===========================================================================
+#                      autogen.sh
+#===========================================================================
+
 srcdir=`dirname $0`
-PKG_NAME="vcdimager"
+test -z "$srcdir" && srcdir=.
+cd $srcdir
 
+test -z "$AUTOMAKE"   && AUTOMAKE=automake
+test -z "$ACLOCAL"    && ACLOCAL=aclocal
+test -z "$AUTOCONF"   && AUTOCONF=autoconf
+test -z "$AUTOHEADER" && AUTOHEADER=autoheader
+test -z "$LIBTOOLIZE" && LIBTOOLIZE=$(which libtoolize glibtoolize 2>/dev/null | head -1)
+test -z "$LIBTOOLIZE" && LIBTOOLIZE=libtoolize #paranoid precaution
 
-if grep "^AM_PROG_LIBTOOL" configure.ac >/dev/null; then
-	echo "Running libtoolize..."
-	libtoolize --force --copy
+# pre-create some dirs / files
+auxdir='.'
+if grep -q "AC_CONFIG_AUX_DIR" configure.ac ; then
+	auxdir="$(grep AC_CONFIG_AUX_DIR configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
+fi
+mkdir -p ${auxdir}
+touch ${auxdir}/config.rpath
+m4dir="$(grep AC_CONFIG_MACRO_DIR configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
+if test -n "$m4dir" ; then
+	mkdir -p ${m4dir}
 fi
 
-echo "Running aclocal $aclocalinclude ..."
-mkdir -p m4
-aclocal
-
-if grep "^AM_CONFIG_HEADER" configure.ac >/dev/null; then
-	echo "Running autoheader..."
-	autoheader --force
+# Get all m4 macros required for configure
+if grep -q LT_INIT configure.ac \
+	|| grep -q AM_PROG_LIBTOOL configure.ac \
+	|| grep -q AC_PROG_LIBTOOL configure.ac ; then
+	echo "- Running $LIBTOOLIZE --copy --force"
+	$LIBTOOLIZE --copy --force || exit 1
 fi
 
-echo "Running automake ..."
-automake --add-missing --copy --force
-echo "Running autoconf ..."
-autoconf --force
+echo "- Running $ACLOCAL"
+$ACLOCAL || exit 1
 
-touch $srcdir/docs/version.texi
-touch $srcdir/docs/version-vcd-info.texi
-touch $srcdir/docs/version-vcdxrip.texi
-test -f $srcdir/docs/stamp-vti && rm $srcdir/docs/stamp-vti
-test -f $srcdir/docs/stamp-1 && rm $srcdir/docs/stamp-1
-test -f $srcdir/docs/stamp-2 && rm $srcdir/docs/stamp-2
-  
+echo "- Running $AUTOHEADER --force"
+# Generate config.h.in
+$AUTOHEADER --force || exit 1
+
+# Generate Makefile.in's
+echo "- Running $AUTOMAKE --add-missing --copy --force"
+$AUTOMAKE --add-missing --copy --force || exit 1
+
+# generate configure
+echo "- Running $AUTOCONF --force"
+$AUTOCONF --force || exit 1
+
+if test "$(ls $m4dir)" = "" ; then
+	rmdir $m4dir
+fi
 rm -rf autom4te.cache
